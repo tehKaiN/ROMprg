4b9118d917eabe20fb69cc806fd9dc5d2a29c83c
KaiN <kain@piwnica.ws>
2017-05-19 17:13:02 +0200

    Implemented basic read command on fw end

--- fw/romprg-firmware/romprg-firmware.ino
+++ fw/romprg-firmware/romprg-firmware.ino
@@ -1,4 +1,9 @@
 
+const uint8_t s_ubSerialBfrMaxLength = 150;
+bool s_bSerialRxReady = false;
+char s_szSerialBfr[s_ubSerialBfrMaxLength];
+uint8_t s_ubSerialBfrLength = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -34,29 +39,63 @@ uint16_t eeReadWord(uint32_t addr) {
 
   // Set control lines lo
   PORTG &= ~(_BV(PG0) | _BV(PG2));
-  delay(10);
+  _delay_us(0.5);
 
   // Read from data lines
-  uint16_t ret;
-  ret = (PINC << 8) | PINA;
+  uint16_t uwRet;
+  uwRet = (PINC << 8) | PINA;
 
   // Set control lines hi
-  PORTG |= _BV(PG0) | _BV(PG2);
+  // PORTG |= _BV(PG0) | _BV(PG2);
   
-  return ret;
+  return uwRet;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  delay(1000);
-  int i;
-  uint16_t w;
-  char bfr[255];
-  for(i = 0; i != 100; ++i) {
-    w = eeReadWord(i);
-    w = (w << 8) | (w >> 8);
-    Serial.write((char*)&w, 2);
-  }
+  serialProcessRx();
   delay(200);
 }
+
+void serialProcessRx() {
+  if(!s_bSerialRxReady)
+    return;
+    
+  uint32_t lStart, lCnt;
+  char szCmd[10];
+  sscanf(s_szSerialBfr, "%s %lu %lu", szCmd, &lStart, &lCnt);
+  if(!strcmp(szCmd, "read")) {
+    // Read mode
+    int i;
+    uint16_t w;
+    char bfr[255];
+    sprintf(bfr, "start: %lu, cnt: %lu", lStart, lCnt);
+    Serial.println(bfr);
+    for(i = lStart; i < lCnt; ++i) {
+      w = eeReadWord(i);
+      w = (w << 8) | (w >> 8);
+      Serial.write((char*)&w, 2);
+    }
+  }
+  else if(!strcmp(szCmd, "write")) {
+    // Write mode
+  }
+  
+  s_bSerialRxReady = false;
+  s_ubSerialBfrLength = 0;
+}
+
+void serialEvent() {
+  // Read message from PC app
+  while(Serial.available() && !s_bSerialRxReady) {
+    char c = (char)Serial.read();
+    s_szSerialBfr[s_ubSerialBfrLength] = c;
+    ++s_ubSerialBfrLength;
+    
+    if(c == '\n') {
+      s_bSerialRxReady = true;
+      s_szSerialBfr[s_ubSerialBfrLength] = '\0';
+      ++s_ubSerialBfrLength;      
+    }
+  }
+}
+
